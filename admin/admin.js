ce2f3cd4a02e8e37214f534da65bc4c4450cb336
Your Name <you@example.com>
2025-06-10 19:47:24 +0300

    Design update

--- admin/admin.js
+++ admin/admin.js
@@ -1,5 +1,7 @@
  const toggleBtn = document.getElementById("toggleMenu");
-  const sidebar = document.getElementById("sidebarMenu");
+const sidebar = document.getElementById("sidebarMenu");
+const body = document.getElementById('Body');
+
 
   toggleBtn.addEventListener("click", () => {
     sidebar.classList.toggle("d-none");
@@ -8,8 +10,10 @@
   function handleResize() {
     if (window.innerWidth >= 768) {
       sidebar.classList.remove("d-none");
+      body.style.marginRight='260px';
     } else {
       sidebar.classList.add("d-none");
+      body.style.marginRight='0';
     }
   }
 
